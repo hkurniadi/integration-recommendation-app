aa3ff3836a1bba0646509089be6062965e2ffd75
Hans Kurniadi <hans.d.k@live.com>
2021-04-25 17:22:17 -0700

    imported bootstrap stylesheet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Components Import
 import SolutionsList from './components/SolutionsList';
 import UserCriteria from './components/UserCriteria';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <div className="App">
-      <SolutionsList></SolutionsList>
-      <UserCriteria></UserCriteria>
+      <Container>
+        <SolutionsList></SolutionsList>
+        <UserCriteria></UserCriteria>  
+      </Container>      
     </div>
   );
 }

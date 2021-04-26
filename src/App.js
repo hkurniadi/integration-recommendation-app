43bd5f3ab7628542cfb1834879fdf33d7f3c99e5
Hans Kurniadi <hans.d.k@live.com>
2021-04-25 17:45:41 -0700

    added Jumbotron welcoming message

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import SolutionsList from './components/SolutionsList';
 import UserCriteria from './components/UserCriteria';
 import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 function App() {
   return (
     <div className="App">
       <Container>
+        <Jumbotron>
+          <h1>Sales Engineering Recommendation Tool</h1>
+          <p>Welcome! To use this tool, enter your integration requirements by selecting the available input checkboxes below</p>
+        </Jumbotron>
         <SolutionsList></SolutionsList>
         <UserCriteria></UserCriteria>  
       </Container>      
